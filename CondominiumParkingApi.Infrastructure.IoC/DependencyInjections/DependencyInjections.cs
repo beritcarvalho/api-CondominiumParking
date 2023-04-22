b630d003fa3886d3bd6ce3b0931c3ea6ca9b7424
Berit Carvalho <beritcarvalho@gmail.com>
2023-04-22 11:16:15 -0300

    - Adicionado o contexto ao scopo da aplicação - Adicionado Extensão de injeção de dependencias

---
+++ CondominiumParkingApi.Infrastructure.IoC/DependencyInjections/DependencyInjections.cs
@@ -0,0 +1,22 @@
+﻿using CondominiumParkingApi.Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CondominiumParkingApi.Infrastructure.IoC.DependencyInjections
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region DataBaseConnection
+
+            services.AddDbContext<DataBaseContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            #endregion
+
+            return services;
+        }
+    }
+}
