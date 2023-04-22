c5d32c57a332972e001aea2c2e24ca06e9b0eb91
Berit Carvalho <beritcarvalho@gmail.com>
2023-04-22 16:00:35 -0300

    - Adicionado ControllerParking - Removido Controller genérico criado pelo aspnet - Removido Classe de exemplo criada pelo aspnet

---
+++ CondominiumParkingApi.Api/Controllers/ParkingController.cs
@@ -0,0 +1,35 @@
+using CondominiumParkingApi.Applications.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CondominiumParkingApi.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/parking")]
+    public class ParkingController : ControllerBase
+    {
+        private readonly IParkedService _parkedService;
+
+        public ParkingController(IParkedService parkedService)
+        {
+            _parkedService = parkedService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var parkingSpaces = await _parkedService.GetAll();
+
+                if (parkingSpaces.Count is 0)
+                    return NotFound();
+
+                return Ok(parkingSpaces);
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
